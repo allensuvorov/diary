d260b523e7e68494f64a14b5cf20ced4bcf81af4
Allen Suvorov <allen.suvorov@gmail.com>
2024-12-28 19:09:07 -0800

    add first handler to return 'welcome to your diary!' to the root url

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Handler - hello\n")
 	fmt.Fprintf(w, "welcome to your diary!\n")
+	log.Print("Handler - buy\n")
 }
 
 func main() {
