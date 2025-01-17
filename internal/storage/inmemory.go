cd31c276f24cef38e17d6c4270e273c545bcb906
Allen Suvorov <allen.suvorov@gmail.com>
2025-01-16 18:12:13 -0800

    store in memory in a map of maps map[int]map[int]entities.Note

--- internal/storage/inmemory.go
+++ internal/storage/inmemory.go
@@ -1,11 +1,13 @@
 package storage
 
+import "github.com/allensuvorov/diary/internal/domain/entities"
+
 type noteInMemoryStorage struct {
-	notes map[int]string
+	notes map[int]map[int]entities.Note
 }
 
 func NewNoteInMemoryStorage() *noteInMemoryStorage {
 	return &noteInMemoryStorage{
-		notes: make(map[int]string),
+		notes: make(map[int]map[int]entities.Note),
 	}
 }
