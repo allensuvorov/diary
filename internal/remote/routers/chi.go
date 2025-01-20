52fd338d910e7b5d552d4be43f90e596097c140a
Allen Suvorov <allen.suvorov@gmail.com>
2025-01-19 18:02:27 -0800

    add TODO to review std lib tools instead of chi

--- internal/remote/routers/chi.go
+++ internal/remote/routers/chi.go
@@ -5,6 +5,9 @@ import (
 	"github.com/go-chi/chi"
 )
 
+// TODO review https://youtu.be/H7tbjKFSg58?si=I_Eyz_sb6Lfj-uWX
+// and if makes sense - remove chi and use standard library tools from go 1.22
+
 func NewRouter(note handlers.NoteHandler) chi.Router {
 	r := chi.NewRouter()
 	r.Get("/", note.Welcome)
